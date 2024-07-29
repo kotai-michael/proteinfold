24443951f8a8c05e664a8166c8b66f6cc4d08a65
jscgh <j.caley@unsw.edu.au>
2024-07-29 17:49:01 +1000

    Updated database links
    	modified:   conf/dbs.config
    	modified:   modules/local/run_alphafold2.nf
    Added variable links in dbs.config and run_alphafold2.nf

---
+++ pf_files/proteinfold_test.sh
@@ -0,0 +1,13 @@
+module load nextflow/23.04.4 java/11
+export SINGULARITY_CACHE_DIR=/srv/scratch/sbf/singularity_cache
+export NXF_SINGULARITY_CACHEDIR=/srv/scratch/sbf/singularity_cache
+
+nextflow run /srv/scratch/z5378336/proteinfold/main.nf \
+    --input samplesheet.csv \
+    --outdir test_out \
+    --mode alphafold2 \
+    --alphafold2_db /data/bio/alphafold \
+    --full_dbs true \
+    --alphafold2_model_preset monomer \
+    --use_gpu true \
+    -profile singularity
---
+++ pf_files/proteinfold_test_cpu.sh
@@ -0,0 +1,9 @@
+nextflow run nf-core/proteinfold -r 1.1.0 \
+    --input samplesheet.csv \
+    --outdir test_out \
+    --mode alphafold2 \
+    --alphafold2_db /data/bio/alphafold \
+    --full_dbs true \
+    --alphafold2_model_preset monomer \
+    --use_gpu false \
+    -profile test
