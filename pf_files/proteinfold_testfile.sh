c993475045f3a14b554c253c41605c9e1d13f66b
jscgh <j.caley@unsw.edu.au>
2024-10-14 15:27:29 +1100

    Initial draft rosettafold-all-atom.nf

--- pf_files/proteinfold_testfile.sh
+++
@@ -1,9 +0,0 @@
-nextflow run nf-core/proteinfold -r 1.1.0 \
-    --input samplesheet.csv \
-    --outdir test_out \
-    --mode alphafold2 \
-    --alphafold2_db /data/bio/alphafold \
-    --full_dbs true \
-    --alphafold2_model_preset monomer \
-    --use_gpu true \
-    -profile test
