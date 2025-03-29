c5da31d6a958275a259009230ee1fe2db00cfdb1
tlitfin <t.litfin@unsw.edu.au>
2025-03-29 20:19:45 +1100

    fix pdb parsing in generate_comparison_report.py

--- bin/generate_comparison_report.py
+++ bin/generate_comparison_report.py
@@ -179,29 +179,36 @@ def align_structures(structures):
 
     return aligned_structures
 
-
-def pdb_to_lddt(pdb_files, generate_tsv):
-    pdb_files_sorted = pdb_files
-    pdb_files_sorted.sort()
+def pdb_to_lddt(struct_files, generate_tsv):
+    struct_files_sorted = struct_files
+    struct_files_sorted.sort()
 
     output_lddt = []
     averages = []
 
-    for pdb_file in pdb_files_sorted:
+    for struct_file in struct_files_sorted:
         plddt_values = []
-        current_resd = []
-        last = None
-        with open(pdb_file, "r") as infile:
-            for line in infile:
-                columns = line.split()
-                if len(columns) >= 11:
-                    if last and last != columns[5]:
-                        plddt_values.append(sum(current_resd) / len(current_resd))
-                        current_resd = []
-                    current_resd.append(float(columns[10]))
-                    last = columns[5]
-            if len(current_resd) > 0:
-                plddt_values.append(sum(current_resd) / len(current_resd))
+
+        if struct_file.endswith('.pdb'):
+            parser = PDB.PDBParser(QUIET=True)
+            suffix = ".pdb"
+        elif struct_file.endswith('.cif'):
+            parser = PDB.MMCIFParser(QUIET=True)
+            suffix = ".cif"
+        else:
+            raise NotImplementedError("Reporting only supported for .pdb and .cif filetypes")
+
+        structure = parser.get_structure("", struct_file)
+
+        for residue in structure.get_residues():
+            res_pLDDT_tot = 0
+            res_atom_count = 0
+
+            for atom in residue.get_atoms():
+                res_atom_count +=1
+                res_pLDDT_tot += atom.get_bfactor()
+
+            plddt_values.append(res_pLDDT_tot/res_atom_count) #residue-level mean for ESMfold atom-level pLDDT
 
         # Calculate the average PLDDT value for the current file
         if plddt_values:
