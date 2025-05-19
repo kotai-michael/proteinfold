42499b39cabd2f5b7a3101576f26f8a1cc2c2bf4
Keiran Rowell <54380465+keiran-rowell-unsw@users.noreply.github.com>
2025-05-19 21:22:12 +1000

    Update bin/extract_metrics.py
    correct doubling of '21' value in AA->int
    
    Co-authored-by: Jose Espinosa-Carrasco <kadomu@gmail.com>

--- bin/extract_metrics.py
+++ bin/extract_metrics.py
@@ -17,7 +17,7 @@ from utils import plddt_from_struct_b_factor
 AA_to_int = {
     "A": 0, "C": 1, "D": 2, "E": 3, "F": 4, "G": 5, "H": 6, "I": 7, "K": 8, "L": 9,
     "M": 10, "N": 11, "P": 12, "Q": 13, "R": 14, "S": 15, "T": 16, "V": 17, "W": 18, "Y": 19,
-    "-": 21, ".": 21
+   ".": 20, "-": 21
 }
 
 def a3m_to_int(a3m_file):
